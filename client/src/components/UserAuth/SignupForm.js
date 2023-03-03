2ae8df7950ec7fabc825460420f786e39301bc63
Stephen <stephen.a.nerby@gmail.com>
2023-03-02 22:12:36 -0500

    Signup form template

--- client/src/components/UserAuth/SignupForm.js
+++ client/src/components/UserAuth/SignupForm.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
 
 function SignupForm() {
+
     return (
-        <div>
-            <h1>Signup</h1>
-        </div>
-    )
+        <form>
+            <label>
+                First Name:
+                <input type="text" name="first_name" />
+            </label>
+            <br />
+            <label>
+                Last Name:
+                <input type="text" name="last_name" />
+            </label>
+            <br />
+            <label>
+                Phone #:
+                <input type="text" name="phone_number" />
+            </label>
+            <br />
+            <label>
+                Email:
+                <input type="email" name="email" />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input type="password" name="password" />
+            </label>
+            <br />
+            <label>
+                Confirm Password:
+                <input type="password" name="password_confirmation" />
+            </label>
+            <br />
+            <button type="submit">Sign Up</button>
+        </form>
+    );
 }
 
-export default SignupForm
+export default SignupForm;
