256a6bec4e0b51c9356628aec4588200fafcd06b
Stephen <stephen.a.nerby@gmail.com>
2023-03-02 22:15:42 -0500

    Toggle button Login/Signup

--- client/src/components/UserAuth/AuthPage.js
+++ client/src/components/UserAuth/AuthPage.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import SignupForm from './SignupForm';
+import LoginForm from './LoginForm';
 
 function AuthPage() {
+    const [isSignup, setIsSignup] = useState(false);
+
+    const handleToggleForm = () => {
+        setIsSignup(!isSignup);
+    };
+
     return (
         <div>
-            <h1>Login/Sign-up</h1>
+            {isSignup ? <SignupForm /> : <LoginForm />}
+            <button onClick={handleToggleForm}>
+                {isSignup ? 'Already have an account? Log in' : 'Need to create an account? Sign up'}
+            </button>
         </div>
-    )
+    );
 }
 
-export default AuthPage
+export default AuthPage;
