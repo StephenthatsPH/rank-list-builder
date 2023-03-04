08211a7c82c8fb70910f28eed61fe77d0f2dca7b
Stephen <stephen.a.nerby@gmail.com>
2023-03-03 23:26:36 -0500

    Remove reviews context

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import NavBar from "./components/NavBar";
 import { UserContext } from "./context/userContext";
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,18 +4,15 @@ import './index.css';
 import App from './App';
 import { HospitalProvider } from './context/hospitalsContext';
 import { ProgramProvider } from './context/programsContext';
-import { ReviewProvider } from './context/reviewsContext';
 import { UserProvider } from './context/userContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-        <HospitalProvider>
-            <ProgramProvider>
-                <ReviewProvider>
-                    <UserProvider>
-                        <App />
-                    </UserProvider>
-                </ReviewProvider>
-            </ProgramProvider>
-        </HospitalProvider>
+    <HospitalProvider>
+        <ProgramProvider>
+            <UserProvider>
+                <App />
+            </UserProvider>
+        </ProgramProvider>
+    </HospitalProvider>
 );
